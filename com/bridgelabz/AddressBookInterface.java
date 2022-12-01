c5f5600b9bf210c6f1413362c992a72b035202c3
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-01 23:47:47 +0530

    Ability to add a new contact to address book

---
+++ com/bridgelabz/AddressBookInterface.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * @author Govardhan Reddy
+ */
+
+public interface AddressBookInterface {
+    /**
+     * create abstract method implement in AddressBook class
+     */
+    void addContactDetails();
+    void printContactDetails();
+}
