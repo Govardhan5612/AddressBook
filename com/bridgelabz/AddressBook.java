f668bda0afb9fc0e9ddf07f34a8a3c7c810f491d
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-24 11:39:48 +0530

    Ability to ensure there is no duplicate of first name in perticular address book

--- com/bridgelabz/AddressBook.java
+++ com/bridgelabz/AddressBook.java
@@ -19,32 +19,50 @@ public class AddressBook implements AddressBookInterface {
         /**
          * Enter the contact details using Scanner object
          */
+
         System.out.print("Enter the first name : ");
-        details.setFirstName(input.next());
-        System.out.print("Enter the last name : ");
-        details.setLastName(input.next());
-        System.out.print("Enter the address : ");
-        details.setAddress(input.next());
-        System.out.print("Enter the city name : ");
-        details.setCity(input.next());
-        System.out.print("Enter the state name : ");
-        details.setState(input.next());
-        System.out.print("Enter the zip(6 numbers) : ");
-        details.setZip(input.next());
-        System.out.print("Enter the Mobile number(10 numbers) : ");
-        details.setPhoneNumber(input.next());
-        System.out.print("Enter the email : ");
-        details.setEmail(input.next());
-        ContactDetails contact1 = new ContactDetails(details.getFirstName(), details.getLastName(), details.getAddress(), details.getCity(), details.getState(), details.getPhoneNumber(), details.getZip(), details.getEmail());
-        listOfContacts.add(contact1);
+        String firstName = input.next();
+        boolean status = false;
+        for (ContactDetails contactDetails : listOfContacts) {
+            if (contactDetails.getFirstName().equals(firstName) == true) {
+                status = true;
+                break;
+            }
+        }
+        if (status == true) {
+            System.out.println(firstName + " already exits");
+            addContactDetails();
+
+        } else {
+            details.setFirstName(firstName);
+            System.out.print("Enter the last name : ");
+            details.setLastName(input.next());
+            System.out.print("Enter the address : ");
+            details.setAddress(input.next());
+            System.out.print("Enter the city name : ");
+            details.setCity(input.next());
+            System.out.print("Enter the state name : ");
+            details.setState(input.next());
+            System.out.print("Enter the zip(6 numbers) : ");
+            details.setZip(input.next());
+            System.out.print("Enter the Mobile number(10 numbers) : ");
+            details.setPhoneNumber(input.next());
+            System.out.print("Enter the email : ");
+            details.setEmail(input.next());
+            ContactDetails contact1 = new ContactDetails(details.getFirstName(), details.getLastName(), details.getAddress(), details.getCity(), details.getState(), details.getZip(), details.getPhoneNumber(), details.getEmail());
+            listOfContacts.add(contact1);
+        }
     }
+
 
     @Override
     public void printContactDetails() {
         /**
          * print the contact details
          */
-        System.out.println(listOfContacts);
+        for (ContactDetails contact : listOfContacts) {
+            System.out.println(contact.toString());
+        }
     }
 
     @Override
@@ -55,11 +73,11 @@ public class AddressBook implements AddressBookInterface {
         for (ContactDetails details : listOfContacts) {
             if (details.getFirstName().equals(firstName)) {
                 listOfContacts.remove(details);
-                number =1;
+                number = 1;
                 break;
             }
         }
-        if (number== 0) {
+        if (number == 0) {
             System.out.println(firstName + " Contact list not found");
         } else {
             addContactDetails();
@@ -76,11 +94,11 @@ public class AddressBook implements AddressBookInterface {
         for (ContactDetails details : listOfContacts) {
             if (details.getFirstName().equals(firstName)) {
                 listOfContacts.remove(details);
-                number=1;
+                number = 1;
                 break;
             }
         }
-        if (number ==0) {
+        if (number == 0) {
             System.out.println(firstName + " Contact list not found");
         } else {
             System.out.println("Delete " + firstName + " contact details");
